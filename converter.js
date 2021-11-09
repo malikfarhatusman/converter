36c0fe4e8417387f45ffab128113cad48a7fbb72
Farhat Usman <malikfarhatusman@gmail.com>
2021-11-09 20:17:44 +0530

    my frist Js app
    its my frist app

---
+++ converter.js
@@ -0,0 +1,26 @@
+const convertTemp = () =>{
+
+        const numTemp = document.getElementById('number').value;
+        const selectVal = document.getElementById('seloptions');
+        const selValues = seloptions.options[selectVal.selectedIndex].value;
+
+        const celToFeh = (cels) =>{
+            let fahrenheit = Math.round((cels *9/5) + 32);
+            return fahrenheit;
+        }
+
+        const fehToCel =(fahr) =>{
+            let celcius = Math.round((fahr - 32) * 5/9);
+            return celcius;
+        }
+
+        let result;
+        if (selValues =='cels'){
+            result = celToFeh(numTemp);
+            document.getElementById('rescontainer').innerHTML= result + ' Fehrenheit';
+        }
+        else{
+            result = fehToCel(numTemp);
+            document.getElementById('rescontainer').innerHTML= result + ' Celcius';
+        }
+}
